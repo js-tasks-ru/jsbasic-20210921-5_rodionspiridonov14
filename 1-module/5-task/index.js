031cf4171cbcce38f6702fadda99584e71e5918a
Rodion Spiridonov <rodionspiridonov14@gmail.com>
2021-10-10 01:08:45 +0300

    Truncate the string and return the value

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
   // ваш код...
+  if (str.length > maxlength) {
+  let b = str.slice(0 ,maxlength - 1)
+  b  += '\…'
+  return b;
+  } else {
+  return str;
+  }
 }
+
