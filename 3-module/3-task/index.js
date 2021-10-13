c215a783796b1930e2a1b22d81b80103c550a5fc
Rodion Spiridonov <rodionspiridonov14@gmail.com>
2021-10-13 23:01:49 +0300

    Made a function for transforming text

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
   // ваш код...
+  if (str == '') {
+    return str
+  } else {
+    const arr = str.split('-')
+    return arr[0] + arr.slice(1).map(item => {
+      return item.charAt(0).toUpperCase() + item.slice(1);
+    } ).join('');
+  }
 }
