25bc3f938c18f980a5128b5b2e9c0ae6a636c27e
Rodion Spiridonov <rodionspiridonov14@gmail.com>
2021-10-14 18:55:40 +0300

    Do a function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const newUl = document.createElement('ul')
+  for (let i = 0; i < friends.length; i++) {
+    newUl.innerHTML = newUl.innerHTML + `<li>${friends[i].firstName} ${friends[i].lastName}</li>`;
+  }
+  return newUl
 }
